9321db58cc116e67d5aab59f076f564f9ebcc2a7
notroyos <logic-masers0k@icloud.com>
2023-03-24 14:29:31 +0100

    correction ortographe dans le dossier controllers > user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/user');
 
 // Créer un nouvel utilisateur
 exports.signup = (req, res, next) => {
